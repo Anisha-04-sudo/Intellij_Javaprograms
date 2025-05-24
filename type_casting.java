a0ee83e4be307e46c4c40ac8059f8cf6d2c85c81
Anisha-04-sudo <anishafaiyaz799@gmail.com>
2025-05-24 21:31:38 +0530

    Add files via upload

---
+++ type_casting.java
@@ -0,0 +1,11 @@
+public class type_casting {
+    public static void main(String[] args){
+        double x = 10.60;
+        int y = (int)x;
+        System.out.println(y);
+
+        int a =9;
+        double b = (double) a;
+        System.out.println(b);
+    }
+}
