98c8d69345ff256105ef29375d558fc09e2a0e80
Anisha-04-sudo <anishafaiyaz799@gmail.com>
2025-05-24 20:10:15 +0530

    Add files via upload

---
+++ datatype_demonstaration.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class datatype_demonstaration {
+    public static void main(String[] args){
+       float a = 5.60f;
+       double b = 8.99;
+       char p = 'A';
+       boolean f = true;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(p);
+        System.out.println(f);
+    }
+}
